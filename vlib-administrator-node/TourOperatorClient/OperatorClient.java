a3efa40c7597d3dc0ab10030754b12080597135d
Oscar Guillen <guillen.usb@gmail.com>
2017-11-13 17:21:42 +0100

    adding class squeleton

---
+++ vlib-administrator-node/TourOperatorClient/OperatorClient.java
@@ -0,0 +1,14 @@
+package TourOperatorClient;
+
+import javax.naming.InitialContext;
+
+import java.util.*;
+
+
+public class OperatorClient {
+
+    public static void main(String[] args) {
+    
+    }
+
+}
